4ea13dd6273d209bc6c29ffef59f540fcc10a273
Andrew McKasson <amckasson90@gmail.com>
2021-05-12 08:33:58 -0400

    First Commit

---
+++ AnneBoleynEscape/ProgramUI.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnneBoleynEscape
+{
+    public class ProgramUI
+    {
+        public bool _bedroomKey = false;
+        //public static Room annesRoom = new Room
+        public void Run()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            { 
+
+            Console.Clear();
+            Console.WriteLine("Anne, things with Henry aren't working out.  He's stuck you in the\n" +
+                "Tower of London until your execution.  You should probably try to get out of here\n" +
+                "while you still can. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Anne, seriously it's time to get moving choose an action:\n" +
+                "1. Go over to the bed.\n" +
+                "2. Go over to the door.\n" +
+                "3. Yell to see if anyone can hear you.");
+
+            string input = Console.ReadLine();
+
+                switch (input.ToLower())
+                {
+                    case "1":
+                    case "one":
+                        BedMethod();
+                        break;
+                    case "2":
+                    case "two":
+                        BedroomDoorMethod();
+                        break;
+                    case "3":
+                    case "three":
+                        YellMethod();
+                        break;
+
+                }
+            }
+        }
+
+        private void BedMethod()
+        {
+            Console.Clear();
+            Console.WriteLine("You walk over to the bed.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What do you want to do Anne?\n" +
+                "1. Pull back the sheets, and try to get some rest.\n" +
+                "2. Check under the pillow.\n" +
+                "3. Walk away from the bed.");
+
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "1":
+                case "one":
+                    PullSheets();
+                    break;
+                case "2":
+                case "two":
+                    LookUnderPillow();
+                    break;
+                case "3":
+                case "three":
+                    WalkAwayFromBed();
+                    break;
+            }
+        }
+
+        private void PullSheets()
+        {
+            Console.Clear();
+            Console.WriteLine("Anne lays and bed and thinks about how she got into this mess.\n" +
+                "She starts to think that maybe Catherine wasn't that bad and Henry might be crazy.\n" +
+                "Anne wakes up to a group of guards in the morning; they take her out to a courtyard\n" +
+                "and cut her head off.  The last thought through her mind is about the overratedness of pious dating.  ");
+            Console.ReadLine();
+        }
+        private void LookUnderPillow()
+        {
+            Console.Clear();
+            Console.WriteLine("Anne pulls the pillow back, and finds a key.\n" +
+                "She wonders why someone would put a key here, and picks it up.");
+            _bedroomKey = true;
+            Console.ReadLine();
+        }
+        private void WalkAwayFromBed()
+        {
+            Console.Clear();
+            Console.WriteLine("Anne has no time for sleep, besides this bed looks like\n" +
+                "its been made for peasants.  Cool pillow though.");
+            Console.ReadLine();
+        }
+
+        private void BedroomDoorMethod()
+        {
+            Console.Clear();
+            if (_bedroomKey == false)
+            {
+                Console.WriteLine("The door looks very sturdy.  Anne tries the handle. \n" +
+                    "She is not surprised to find that its locked.  Maybe there's a key somewhere.");
+            }
+            else 
+            {
+                Console.WriteLine("The key from under the pillow fits the lock.  Anne uses it to leave the room.");
+            }
+            Console.ReadLine();
+        }
+
+        private void YellMethod()
+        {
+            Console.Clear();
+            Console.WriteLine("Anne starts yelling like a lunatic, no one seems to react.\n" +
+                "She begins to wonder if there are any guards outside");
+            Console.ReadLine();
+        }
+    }
+}
