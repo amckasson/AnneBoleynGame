4747e90bc204ba54c364185860ddde59b0d777b8
Andrew McKasson <amckasson90@gmail.com>
2021-05-13 10:03:23 -0400

    Touch Ups and little fixes

--- AnneBoleynEscape/ProgramUI.cs
+++ AnneBoleynEscape/ProgramUI.cs
@@ -20,14 +20,14 @@ namespace AnneBoleynEscape
             Console.Clear();
             Console.WriteLine("Anne, things with Henry aren't working out.  He's stuck you in the\n" +
                 "Tower of London until your execution.  You should probably try to get out of here\n" +
-                "while you still can. ");
+                "while you still can.\n\n ");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
 
             while (_keepRunning)
             {
                 Console.Clear();
-                Console.WriteLine("Anne, seriously it's time to get moving choose an action:\n" +
+                Console.WriteLine("Anne, seriously it's time to get moving, choose an action:\n\n" +
                     "1. Go over to the bed.\n" +
                     "2. Go over to the door.\n" +
                     "3. Yell to see if anyone can hear you.");
@@ -56,12 +56,12 @@ namespace AnneBoleynEscape
         private void BedMethod()
         {
             Console.Clear();
-            Console.WriteLine("You walk over to the bed.\n" +
-                "While not as comfortable as those in the royal quarters, it's better than the straw-filled mattresses used by the commoners.");
+            Console.WriteLine("You walk over to the bed.\n\n" +
+                "While not as comfortable as those in the royal quarters, it's better than the\n straw-filled mattresses used by the commoners.\n");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("What do you want to do Anne? Choose a number:\n" +
+            Console.WriteLine("What do you want to do Anne?\n\n Choose a number:\n\n" +
                 "1. Pull back the sheets, and try to get some rest.\n" +
                 "2. Check under the pillow.\n" +
                 "3. Walk away from the bed.");
@@ -104,7 +104,7 @@ namespace AnneBoleynEscape
         {
             Console.Clear();
             Console.WriteLine("Anne pulls the pillow back, and finds a key.\n" +
-                "She wonders why someone would put a key here, and picks it up.");
+                "She wonders why someone would put a key here, and picks it up.\n");
             _bedroomKey = true;
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
@@ -113,7 +113,7 @@ namespace AnneBoleynEscape
         {
             Console.Clear();
             Console.WriteLine("Anne has no time for sleep, besides this bed looks like\n" +
-                "its been made for peasants.  Cool pillow though.");
+                "its been made for peasants.  Cool pillow though.\n\n");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
@@ -124,12 +124,12 @@ namespace AnneBoleynEscape
             if (_bedroomKey == false)
             {
                 Console.WriteLine("The door looks very sturdy.  Anne tries the handle. \n" +
-                    "She is not surprised to find that its locked.  Maybe there's a key somewhere.");
+                    "She is not surprised to find that its locked.  Maybe there's a key somewhere.\n\n");
                 Console.WriteLine("Press any key to continue...");
             }
             else
             {
-                Console.WriteLine("The key from under the pillow fits the lock.  Anne uses it to leave the room.");
+                Console.WriteLine("The key from under the pillow fits the lock.  Anne uses it to leave the room.\n\n");
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadLine();
                 //Test
@@ -143,7 +143,7 @@ namespace AnneBoleynEscape
         {
             Console.Clear();
             Console.WriteLine("Anne starts yelling like a lunatic, no one seems to react.\n" +
-                "She begins to wonder if there are any guards around.");
+                "She begins to wonder if there are any guards around.\n\n");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
@@ -152,8 +152,8 @@ namespace AnneBoleynEscape
         {
             Console.Clear();
             Console.WriteLine("Anne makes it out of the Room!\n" +
-                "The hallway is dark. There seems to be a dead end to the left.\n But there is a door in front of you, and one down hall to the right.\n" +
-                "What do you want to do?Choose a number:\n" +
+                "The hallway is dark. There seems to be a dead end to the left.\n But there is a door in front of you, and one down the hall to the right.\n\n" +
+                "What do you want to do? Choose a number:\n\n" +
                 "1. Try the door in front of me.\n" +
                 "2. Try the door to the right. ");
 
@@ -193,8 +193,9 @@ namespace AnneBoleynEscape
             Console.Clear();
             Console.WriteLine("Anne you made it into the next room. \n" +
                 "There doesn't seem to be anyone in here.\n" +
-                "You can see that there are two more doors; one to left, and another to the right.\n" +
-                "What door would you like to try? Choose a number:\n" +
+                "The room is lined with antique suits of armour, rusted and forgotten.\n Anne" +
+                " can see that there are two more doors; one to left, and another to the right.\n\n" +
+                "What door would you like to try? Choose a number:\n\n" +
                 "1. Door to the right.\n" +
                 "2. Door to the left.");
 
@@ -225,13 +226,14 @@ namespace AnneBoleynEscape
                 "The only light shining inside coming from the room she left behind her.\n" +
                 "She begins to slowly climb the stairs being careful not to fall.\n" +
                 "She comes to a dead end thats been sealed off.  Putting her ear to the wall\n" +
-                "she thinks she can hear soft crying.  She turns heading back down the stair knowing this is not the right way.");
+                "she thinks she can hear soft crying. \"Nope..\"  She thinks as she turns\n" +
+                "heading back down the stair knowing this is not the right way.\n");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Now back in the room below, she sees the only is to continue on to the door across the room that she has not tried yet; hoping that it opens\n" +
-                "Choose a number:\n" +
-                "1. Try the other door.\n" 
+            Console.WriteLine("Now back in the room below, she sees the only way to continue on is\nthrough door across the room that she has not tried yet; she PRAYS that it opens\n\n" +
+                "Choose a number:\n\n" +
+                "1. Try the other door.\n"
                 //"2. Try yelling to see who comes."
                 );
             //Console.ReadLine();
@@ -246,8 +248,8 @@ namespace AnneBoleynEscape
                     break;
                 //case "2":
                 //case "Two":
-                    //YellMethod();
-                    //break;
+                //YellMethod();
+                //break;
                 default:
                     Console.WriteLine("Please choose a valid number");
                     Console.ReadLine();
@@ -262,8 +264,8 @@ namespace AnneBoleynEscape
             Console.WriteLine("Miraculously, the door opens and leads to a winding starcase going down.\n" +
                 "Anne approaches the top of the staircase that corkscrews down to the bottom floor of the tower.\n" +
                 "As she descends, she can hear a crowd cheering outside in the courtyard. Who could be celebrating such a dire situation?\n" +
-                "As she reaches the bottom of the stairs, there is a door to her left, and the double door leading out to the courtyard.\n" +
-                " Choose a number to explore that room:\n" +
+                "As she reaches the bottom of the stairs, there is a door to her left, and the double door leading out to the courtyard.\n\n" +
+                " Choose a number to explore that room:\n\n" +
                 "1. Door to the left\n" +
                 "2. Double doors straight ahead");
 
@@ -292,7 +294,7 @@ namespace AnneBoleynEscape
             Console.Clear();
             Console.WriteLine("The door swings inward onto a large, lavishly decorated room. Rows upon rows of gaudy outfits hang from large racks.\n" +
                 "The cloths are brightly colored, and are for a very large man. Anne realizes she has stumbled into one of Henry's many rooms,\n" +
-                "each one displaying his many colorful outfits for any occasion. An idea slowly dawns on Anne. Choose a number:\n" +
+                "each one displaying his many colorful outfits for any occasion. An idea slowly dawns on Anne.\n\n Choose a number:\n\n" +
                 "1. No, it's a stupid idea. Best not to get caught in here!\n" +
                 "2. Put on one of Henry's outfits in an attempt to disguise yourself.");
 
@@ -310,7 +312,7 @@ namespace AnneBoleynEscape
                     Console.WriteLine("Anne stands on a stool and dons a large tunic. Looking in the mirror, she realizes that this is only going to work\n" +
                         "as long as she's standing on the stool. Henry is much taller than her!\n" +
                         "Suddenly, a flash of movement in the mirror catches her eye, but too late!" +
-                        "\n\n\nA French assassin, mistaking her for Henry, has stabbed HER in the back instead!" );
+                        "\n\n\nA French assassin, mistaking her for Henry, has stabbed HER in the back instead!");
                     Console.ReadLine();
                     YouDied();
                     break;
@@ -327,8 +329,8 @@ namespace AnneBoleynEscape
             Console.Clear();
             Console.WriteLine("Back at the bottom of the stairs, your only option now is to go through the double doors, out to the noisy courtyard.\n" +
                 "But... you are starting to suspect something isn't right here. Is this some sort of sick surprise from Henry?\n" +
-                "Your birthday is coming up... but he's also not been very pleased with you lately...\n" +
-                "Choose a number:\n" +
+                "Your birthday is coming up... but he's also not been very pleased with you lately...\n\n" +
+                "Choose a number:\n\n" +
                 "1. Proceed through the double doors into the courtyard, ready for your big party!\n" +
                 "2. Pray for guidance.");
 
@@ -348,7 +350,7 @@ namespace AnneBoleynEscape
                 default:
                     Console.WriteLine("You hesistate at the turning point!");
                     YouDied();
-                        break;
+                    break;
             }
         }
 
@@ -357,7 +359,7 @@ namespace AnneBoleynEscape
             Console.Clear();
             Console.WriteLine("As Anne steps out into the bright morning sunlight, she can see a huge crowd forming a large, semicircle around a central object.\n" +
                 "As she draws closer, two large men wearing black hoods obscuring their faces grab each of her arms. Too late she realizes that she has willingly\n" +
-                "walked herself to her own execution!");
+                "walked herself to her own execution!\n\n");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
 
